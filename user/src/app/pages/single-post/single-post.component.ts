6a770a29b51864920967d0254f080e0eb6a6d359
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-01-16 18:19:52 +0530

    created components

---
+++ user/src/app/pages/single-post/single-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-post.component.html',
+  styleUrl: './single-post.component.css'
+})
+export class SinglePostComponent {
+
+}
---
+++ user/src/app/pages/terms-and-condition/terms-and-condition.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-and-condition',
+  standalone: true,
+  imports: [],
+  templateUrl: './terms-and-condition.component.html',
+  styleUrl: './terms-and-condition.component.css'
+})
+export class TermsAndConditionComponent {
+
+}
