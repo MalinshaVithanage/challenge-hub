c6eef2fa22d6c9737bd1b8f8b3e1eb847a24f3ba
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-01-16 19:49:48 +0530

    Done: Setup

--- user/src/app/app.routes.ts
+++ user/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import path from 'path';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'category', component:SingleCategoryComponent},
+    {path:'post',component:SinglePostComponent},
+    {path:'about', component:AboutUsComponent},
+    {path:'terms-conditions', component:TermsAndConditionComponent},
+    {path:'contact', component:ContactUsComponent}
+
+];
