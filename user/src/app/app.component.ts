71db0e41d6a94953b41953b45d172e47b65309e8
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-01-18 11:12:42 +0530

    Set UP done

--- user/src/app/app.component.ts
+++ user/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layouts/header/header.component";
+import { CategoryNavbarComponent } from "./layouts/category-navbar/category-navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent]
 })
 export class AppComponent {
   title = 'user';
