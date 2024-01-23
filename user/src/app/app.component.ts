2a3546de321beaf41a075f45216bd9a0113d8f2f
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-01-23 23:02:45 +0530

    Done- footer

--- user/src/app/app.component.ts
+++ user/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layouts/header/header.component";
 import { CategoryNavbarComponent } from "./layouts/category-navbar/category-navbar.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'user';
