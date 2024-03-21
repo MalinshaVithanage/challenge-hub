de4cf36484267695cb762358c26c7e5e80562a2b
malinshav <malikgvitha@gmail.com>
2024-03-21 14:46:22 +0530

    Post, Comment section done
    UI finished

--- user/src/app/pages/single-post/single-post.component.ts
+++ user/src/app/pages/single-post/single-post.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from "../../layouts/post-card/post-card.component";
 import { CommentFormComponent } from "../../comments/comment-form/comment-form.component";
+import { CommentListComponent } from "../../comments/comment-list/comment-list.component";
 
 @Component({
     selector: 'app-single-post',
     standalone: true,
     templateUrl: './single-post.component.html',
     styleUrl: './single-post.component.css',
-    imports: [PostCardComponent, CommentFormComponent]
+    imports: [PostCardComponent, CommentFormComponent, CommentListComponent]
 })
 export class SinglePostComponent {
 
